1bb831145e875014c95b483c8a7c688c1c0c1ffc
Raul Duarte Pereira <raulduartepe@gmail.com>
2021-04-20 02:30:52 -0300

    add: sentry and rate limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -10,6 +10,8 @@ import 'express-async-errors';
 import createConnection from '@shared/infra/typeorm';
 
 import uploadConfig from '@config/upload';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
 import swaggerSetup from '../../../swagger.json';
 import { errorTratment } from './middlewares/errorTratment';
@@ -21,6 +23,19 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSetup));
@@ -28,6 +43,8 @@ app.use('/uploads', express.static(`${uploadConfig.tmpFolder}`));
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(errorTratment);
 
