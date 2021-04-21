7fcc55b74992212c6387839b864812dfe010e836
Raul Duarte Pereira <raulduartepe@gmail.com>
2021-04-21 01:16:08 -0300

    ci: add script test:ci

--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -1,11 +1,10 @@
 import { hash } from 'bcrypt';
-import { getConnection } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
 import createConnection from '../index';
 
 async function create() {
-  const connection = await createConnection('localhost');
+  const connection = await createConnection();
 
   const id = uuidV4();
   const password = await hash('admin', 8);
